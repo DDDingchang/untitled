58c369278cd9207c5832b3897dc9731e0eebe6ef
DDDingchang <1069848230@qq.com>
2021-02-23 22:51:57 +0800

    commit dev2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	 // write your code here
         // write your code here2
-        System.out.println("x");
-        System.out.println("x");
-        System.out.println("x");
-        System.out.println("x");
-        System.out.println("x");
-        System.out.println("x");
         System.out.println("dev");
     }
 }
